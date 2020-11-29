6df77b05b4b4c72868a777ef6ab103ab2692c29e
mJaJksJ <maksim.m00@mail.ru>
2020-11-29 19:36:30 +0300

    first test

---
+++ Iris/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("431b80e9-ac91-45f3-b6f8-b1b1eb21016d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Iris/UnitTests/UnitTestUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTestUser
+    {
+        [TestMethod]
+        public void TestEquals()
+        {
+            //Arrange
+            IrisLib.User expectedUser1 = new IrisLib.User(111, "Ivan", "Vinogradov", "FunnySurname", 0, "Vina", "qwerty123");
+            IrisLib.User expectedUser2 = new IrisLib.User()
+            {
+                ID = 111,
+                Name = "Ivan",
+                Surname = "Vinogradov",
+                Nickname = "FunnySurname",
+                Age = 0,
+                Login = "Vina",
+                Password = "qwerty123"
+            };
+
+            //Act
+            bool isEquals = expectedUser1.Equals(expectedUser2);
+
+            //Assert
+            Assert.AreEqual(isEquals, true);
+
+        }
+    }
+}
