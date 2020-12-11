c0aadd6ba9e2fadb7c61b84c042ff21a4604f3c2
daria-karam <tirgi.mail@gmail.com>
2020-12-11 21:58:37 +0300

    some cosmetic changes

--- Iris/IrisClient/Model/MainWindow.xaml.cs
+++ Iris/IrisClient/Model/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.IO;
 //using System.Windows.Media.Imaging;
 //using System.Windows.Navigation;
 //using System.Windows.Shapes;
-using IrisClient.ServiceChat;
+//using IrisClient.ServiceChat;
 using IrisLib;
 
 namespace IrisClient
